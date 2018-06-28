57721479100642486f47d5196eb36de9f0f30347
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-06-28 11:29:52 +0100

    Tidy host config

--- config/initializers/laa_fee_calculator_client.rb
+++ config/initializers/laa_fee_calculator_client.rb
@@ -1,5 +1,3 @@
-# FIXME: testing fee calculator integration against local server
-# client gem will default to
 LAA::FeeCalculator.configure do |config|
   config.host = ENV['laa_fee_calculator_host']
 end
