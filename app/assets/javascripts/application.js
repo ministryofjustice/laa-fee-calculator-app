cf3bda2cbedf008055d634994a8898b8db1a686b
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-06-28 16:11:36 +0100

    Use boostrap row, col and form-control for grid

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 // removed require rails-ujs
 
 //
-// = require jquery
-// = require jquery_ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
 //= require turbolinks
 //= require bootstrap-sprockets
